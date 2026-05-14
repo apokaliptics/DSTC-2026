f35d6748f3ac29c6084aece07c48d1e4b54ec17a
apocalypse <minhkietytlhp@gmail.com>
2026-05-15 00:14:22 +0700

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-
 function customAssetResolver() {
   return {
     name: 'custom-asset-resolver',
@@ -17,10 +16,11 @@ function customAssetResolver() {
 }
 
 export default defineConfig({
+  // ADD THIS LINE BELOW
+  base: '/DSTC-2026/', 
+  
   plugins: [
     customAssetResolver(),
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react({
       babel: {
         compact: false,
@@ -30,11 +30,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
-
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
